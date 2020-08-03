c34b6a87acaa374391a089cb1b7f28f86c9fedaa
Juan L <lauredojuan@gmail.com>
2020-08-03 17:07:59 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,50 +1,74 @@
 import React from "react";
-import { ListTask } from "./listtask"
+import { ListTask } from "./listtask";
 
 export class Home extends React.Component {
+	// create constructor to store variables
 
-// create constructor to store variables 
+	// create state w/ empty array to store tasks
 
-// create state w/ empty array to store tasks
+	// create a state variable for current task
 
-// create a state variable for current task
+	// input field containing an event (key up) to store a new task
 
-// input field containing an event (key up) to store a new task
+	// create ul li list
 
-// create ul li list
+	// create map function to include elements in the array list
 
-// create map function to include elements in the array list
+	// include 'delete' function and onclick event inside each li element with contional rendering
 
-// include 'delete' function and onclick event inside each li element with contional rendering
+	constructor() {
+		super();
+		this.state = {
+			task: "",
+			taskList: []
+		};
+	}
+	updateTask = event => {
+		this.setState({ task: event.target.value });
+	};
 
-    constructor(){
-        super();
-        this.state = {
-             task: '',
-             taskList: []
-        }
-    }
-   updateTask = (event) => {
-       task.setState({ task: event.target.value})
-   }
+	saveTask = event => {
+		if (event.keyCode === 13) {
+			const newArray = [...this.state.taskList]; //spread operator in this line a copy of taskList is created and named newArray
+			newArray.push(this.state.task);
+			this.setState({
+				taskList: newArray,
+				task: ""
+			});
+		}
+	};
 
-    saveTask = () => {
+	deleteFunctionHandler = id => {
+		const returnArray = this.state.taskList.filter(
+			(i, index) => index !== id
+		);
+		this.setState({ taskList: returnArray });
+	};
 
-    }
-
-
-
-
-
-    render(){
-        return (
-                <div className="container">
-                    <h1>ToDos</h1>
-                    <input value={this.state.task} onChange={this.updateTask}   onKeyUp={this.saveTask} placeholder="What needs to be done?"/>
-                    <ul>
-                        <ListTask/>
-                    </ul>
-                </div>
-            );
-    }
+	render() {
+		console.log(this.state.taskList);
+		return (
+			<div className="container">
+				<h1>ToDos</h1>
+				<input
+					value={this.state.task}
+					onChange={this.updateTask}
+					onKeyUp={this.saveTask}
+					placeholder="What needs to be done?"
+				/>
+				<ul>
+					{this.state.taskList.map((liContent, index) => {
+						return (
+							<ListTask
+								key={index}
+								task={liContent}
+								deleteFunction={this.deleteFunctionHandler}
+								id={index}
+							/>
+						);
+					})}
+				</ul>
+			</div>
+		);
+	}
 }
--- src/js/component/listtask.js
+++ src/js/component/listtask.js
@@ -1,28 +1,22 @@
 import React from "react";
-
+import { PropTypes } from "prop-types";
 
 export function ListTask(props) {
-
-    return (
+	return (
 		<li key={props.id}>
-           {props.task}<span><i className="fa fa-trash-o"  onClick={props.deleteFunction}></i></span>
-        </li>
-    )
-};
-
-ListTask.propTypes = {
-    id: PropTypes.number,
-    task: PropTypes.string,
-    deleteFunction: PropTypes.func
+			{props.task}
+			<span>
+				<i
+					className="fa fa-trash-o"
+					onClick={() => props.deleteFunction}
+				/>
+			</span>
+		</li>
+	);
 }
 
-
-
-
-
-
-
-
-
-    
-
+ListTask.propTypes = {
+	id: PropTypes.number,
+	task: PropTypes.string,
+	deleteFunction: PropTypes.func
+};
